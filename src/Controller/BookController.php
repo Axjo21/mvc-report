239ee65dd3f44712bbfc267d0cd5bb4fda9588b4
Axjo21 <117208303+Axjo21@users.noreply.github.com>
2024-09-08 21:25:48 +0200

    Create book class and integrate Doctrine

---
+++ src/Controller/BookController.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\Book;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\BookRepository;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class BookController extends AbstractController
+{
+    # HOME
+    #[Route('/library', name: 'library')]
+    public function index(): Response
+    {
+        return $this->render('book/home.html.twig', [
+            'controller_name' => 'BookController',
+        ]);
+    }
+
+
+    # CREATE (form)
+    #[Route('/library/create_new', name: 'library_create_new')]
+    public function createNewBook(): Response
+    {
+        return $this->render('book/create.html.twig', [
+            'controller_name' => 'BookController',
+        ]);
+    }
+
+
+    # CREATE (actually create)
+    #[Route('/library/create', name: 'library_create')]
+    public function createBook(
+        ManagerRegistry $doctrine,
+        Request $request
+    ): Response {
+
+        $entityManager = $doctrine->getManager();
+        $book = new Book();
+
+        #$book = $entityManager->getRepository(Book::class)->findOneBy([], ['id' => 'DESC']);
+
+        $title = $request->request->get('book_title');
+        $author = $request->request->get('book_author');
+        $isbn = $request->request->get('book_isbn');
+
+        $book->setTitle($title);
+        $book->setAuthor($author);
+        $book->setISBN($isbn);
+
+        # persist = eventually save, flush = actually save
+        $entityManager->persist($book);
+        $entityManager->flush();
+
+        return new Response('Saved new book with id '.$book->getId());
+    }
+
+
+    # VIEW ALL
+    #[Route('/library/view', name: 'library_view_all')]
+    public function viewAllBook(
+        BookRepository $bookRepository
+    ): Response {
+        $books = $bookRepository->findAll();
+
+        $data = [
+            'books' => $books
+        ];
+
+        return $this->render('book/view.html.twig', $data);
+    }
+
+    # VIEW SINGLE
+    #[Route('/library/view/{id}', name: 'library_view_by_id')]
+    public function viewBookById(
+        BookRepository $bookRepository,
+        int $id
+    ): Response {
+        $book = $bookRepository
+            ->find($id);
+
+        $data = [
+            'book' => $book
+        ];
+
+        return $this->render('book/single.html.twig', $data);
+    }
+
+
+    # UPDATE
+    #[Route('/library/update/{id}/{value}', name: 'library_update')]
+    public function updateBook(
+        ManagerRegistry $doctrine,
+        int $id,
+        int $title
+    ): Response {
+        $entityManager = $doctrine->getManager();
+        $book = $entityManager->getRepository(Book::class)->find($id);
+
+        if (!$book) {
+            throw $this->createNotFoundException(
+                'No book found for id '.$id
+            );
+        }
+
+        $book->setTitle($title);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('library_show_all');
+    }
+
+
+    # DELETE
+    #[Route('/library/delete/{id}', name: 'library_delete_by_id')]
+    public function deleteBookById(
+        ManagerRegistry $doctrine,
+        int $id
+    ): Response {
+        $entityManager = $doctrine->getManager();
+        $book = $entityManager->getRepository(Book::class)->find($id);
+
+        if (!$book) {
+            throw $this->createNotFoundException(
+                'No book found for id '.$id
+            );
+        }
+
+        $entityManager->remove($book);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('library_show_all');
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # SHOW ALL (JSON respons)
+    #[Route('/library/show', name: 'library_show_all')]
+    public function showAllBook(
+        BookRepository $bookRepository
+    ): Response {
+        $books = $bookRepository
+            ->findAll();
+        $response = $this->json($books);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+
+
+    # SHOW SINGLE (JSON respons)
+    #[Route('/library/show/{id}', name: 'library_by_id')]
+    public function showBookById(
+        BookRepository $bookRepository,
+        int $id
+    ): Response {
+        $book = $bookRepository
+            ->find($id);
+
+        return $this->json($book);
+    }
+
+
+    # EJ ANVÄND
+    #[Route('/library/view/{value}', name: 'library_view_minimum_value')]
+    public function viewBookWithMinimumValue(
+        BookRepository $bookRepository,
+        int $value
+    ): Response {
+        $books = $bookRepository->findByMinimumValue($value);
+
+        $data = [
+            'books' => $books
+        ];
+
+        return $this->render('book/view.html.twig', $data);
+    }
+
+
+    # EJ ANVÄND
+    #[Route('/library/show/min/{value}', name: 'library_by_min_value')]
+    public function showBookByMinimumValue(
+        BookRepository $bookRepository,
+        int $value
+    ): Response {
+        $products = $bookRepository->findByMinimumValue2($value);
+
+        return $this->json($products);
+    }
+
+}
---
+++ src/Repository/BookRepository.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Book;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Book>
+ */
+class BookRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Book::class);
+    }
+
+
+    # ANPASSA FÖR BOK TABELL
+    /**
+     * Find all books having a value above the specified one.
+     * 
+     * @return Book[] Returns an array of Book objects
+     */
+    public function findByMinimumValue($value): array
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.value >= :value')
+            ->setParameter('value', $value)
+            ->orderBy('p.value', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    # ALTERNATIV TILL OVAN (?)
+    /**
+     * Find all producs having a value above the specified one with SQL.
+     * 
+     * @return [][] Returns an array of arrays (i.e. a raw data set)
+     */
+    public function findByMinimumValue2($value): array
+    {
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = '
+            SELECT * FROM product AS p
+            WHERE p.value >= :value
+            ORDER BY p.value ASC
+        ';
+
+        $resultSet = $conn->executeQuery($sql, ['value' => $value]);
+
+        return $resultSet->fetchAllAssociative();
+    }
+
+//    /**
+//     * @return Book[] Returns an array of Book objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('b')
+//            ->andWhere('b.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('b.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Book
+//    {
+//        return $this->createQueryBuilder('b')
+//            ->andWhere('b.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
